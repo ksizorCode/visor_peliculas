8205ea0358fd59186e3c78c519f7062d59e3f678
Miguel <miguelestebn@gmail.com>
2023-07-10 19:05:08 +0200

    aa

---
+++ HTML y PHP/en PHP/index.php
@@ -0,0 +1,18 @@
+
+
+<?
+//esto es un comentario de línea
+
+/*
+Esto es un
+comentario de párrafo
+ */
+
+$nombre ="Pepito";
+$apellido ="Martínez";
+$nacim=1999;
+?>
+
+<h1>1 Pepito Martinez nacido tiene 24 años</h1>
+
+<h1>2 <? echo $nombre?> <? echo $apellido?> nacido tiene <? echo (2023-$nacim)?> años</h1>
